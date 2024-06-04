95961204bfa6d64fccaf73b3600f56fc8db28f86
Pavel <Pavel.Pasechnik@gmail.com>
2024-06-04 16:12:29 +0300

    fix error

--- src/helpers/validateBody.js
+++ src/helpers/validateBody.js
@@ -1,6 +1,6 @@
-import HttpError from "./HttpError.js";
+import HttpError from './HttpError.js';
 
-const validateBody = (schema) => {
+const validateBody = schema => {
   const func = (req, _, next) => {
     const { error } = schema.validate(req.body);
     if (error) {
