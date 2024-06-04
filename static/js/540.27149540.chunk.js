b58fddc666d677227a6ddff5458776a72249b6be
pavel-pasechnik <Pavel.Pasechnik@gmail.com>
2024-06-04 13:13:19 +0000

    Deploying to gh-pages from @ pavel-pasechnik/Task-Pro@95961204bfa6d64fccaf73b3600f56fc8db28f86 🚀

---
+++ static/js/540.27149540.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktask_pro=self.webpackChunktask_pro||[]).push([[540],{540:(s,a,e)=>{e.r(a),e.d(a,{default:()=>k});var r=e(579);const k=()=>(0,r.jsx)("h1",{children:"Boards"})}}]);
+//# sourceMappingURL=540.27149540.chunk.js.map
